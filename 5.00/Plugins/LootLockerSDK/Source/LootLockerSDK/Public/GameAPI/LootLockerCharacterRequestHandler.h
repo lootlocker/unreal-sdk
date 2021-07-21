024a324426bc4af4ba7eb02776460b7b4d8ba0e4
Hoffa <robin.hofstrom@gmail.com>
2021-07-21 07:03:28 +0200

    Added missing UPROPERTY for struct FLootLockerListCharacterTypesResponse in LootLockerCharacterRequestHandler.h
    Added missing uproperty for struct FLootLockerListCharacterTypesResponse.

--- 5.00/Plugins/LootLockerSDK/Source/LootLockerSDK/Public/GameAPI/LootLockerCharacterRequestHandler.h
+++ 5.00/Plugins/LootLockerSDK/Source/LootLockerSDK/Public/GameAPI/LootLockerCharacterRequestHandler.h
@@ -107,18 +107,19 @@ struct FLootLockerEquipUniversalAssetToCharacterRequest {
 };
 
 USTRUCT(BlueprintType)
-struct FLootLockerListCharacterTypesResponse : public FLootLockerResponse {
-	GENERATED_BODY()
-	TArray<struct FLootLockerCharacterTypes> character_types;
-};
-
-USTRUCT(BlueprintType)
 struct FLootLockerCharacterTypes
 {
 	GENERATED_BODY()
 	int32 id;
 	bool is_default;
 	FString name;
+};
+
+USTRUCT(BlueprintType)
+struct FLootLockerListCharacterTypesResponse : public FLootLockerResponse {
+	GENERATED_BODY()
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLocker")
+	TArray<struct FLootLockerCharacterTypes> character_types;
 };
 
 DECLARE_DYNAMIC_DELEGATE_OneParam(FPCharacterLoadoutResponseBP, FLootLockerCharacterLoadoutResponse, Var);
