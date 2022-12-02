ccef76cdfe571d9309cc5def1d201de7f0f9a123
Erik Bylund <erik@lootlocker.io>
2022-12-02 11:55:20 +0100

    Update Leaderboard interaction to be by key only

--- LootLockerSDK/Source/LootLockerSDK/Private/GameAPI/LootLockerLeaderboardRequestHandler.cpp
+++ LootLockerSDK/Source/LootLockerSDK/Private/GameAPI/LootLockerLeaderboardRequestHandler.cpp
@@ -13,42 +13,34 @@ ULootLockerLeaderboardRequestHandler::ULootLockerLeaderboardRequestHandler()
 	HttpClient = NewObject<ULootLockerHttpClient>();
 }
 
-
-
 void ULootLockerLeaderboardRequestHandler::GetMemberRank(const FLootLockerGetMemberRankRequest& MemberRequest, const FLootLockerGetMemberRankResponseBP& OnCompletedRequestBP, const FLootLockerGetMemberRankResponseDelegate& OnCompletedRequest)
 {
-	LLAPI<FLootLockerGetMemberRankResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::GetMemberRank, { MemberRequest.leaderboardId, MemberRequest.member_id },EmptyQueryParams,OnCompletedRequestBP, OnCompletedRequest);
+	LLAPI<FLootLockerGetMemberRankResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::GetMemberRank, { MemberRequest.leaderboard_key, MemberRequest.member_id }, EmptyQueryParams, OnCompletedRequestBP, OnCompletedRequest);
 }
 
-void ULootLockerLeaderboardRequestHandler::GetByListOfMembers(const FLootLockerGetByListMembersRequest& GetScoreByListRequests, int LeaderboardId, const FLootLockerGetByListOfMembersResponseBP& OnCompletedRequestBP, const FLootLockerGetByListOfMembersResponseDelegate& OnCompletedRequest)
+void ULootLockerLeaderboardRequestHandler::GetByListOfMembers(const FLootLockerGetByListMembersRequest& GetScoreByListRequests, FString LeaderboardKey, const FLootLockerGetByListOfMembersResponseBP& OnCompletedRequestBP, const FLootLockerGetByListOfMembersResponseDelegate& OnCompletedRequest)
 {
-	LLAPI<FLootLockerGetByListOfMembersResponse>::CallAPI(HttpClient, GetScoreByListRequests, ULootLockerGameEndpoints::GetByListOfMembers, { LeaderboardId },EmptyQueryParams,OnCompletedRequestBP, OnCompletedRequest);
+	LLAPI<FLootLockerGetByListOfMembersResponse>::CallAPI(HttpClient, GetScoreByListRequests, ULootLockerGameEndpoints::GetByListOfMembers, { LeaderboardKey }, EmptyQueryParams, OnCompletedRequestBP, OnCompletedRequest);
 }
 
 void ULootLockerLeaderboardRequestHandler::GetScoreList(const FLootLockerGetScoreListRequest& GetScoreListRequests, const FLootLockerGetScoreListResponseBP& OnCompletedRequestBP, const FLootLockerGetScoreListResponseDelegate& OnCompletedRequest)
 {
-	if (GetScoreListRequests.after >= 0)
-	{
-		LLAPI<FLootLockerGetScoreListResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::GetScoreListAfter, { GetScoreListRequests.leaderboardId, GetScoreListRequests.count, GetScoreListRequests.after },EmptyQueryParams,OnCompletedRequestBP, OnCompletedRequest);
-	}
-	else
-	{
-		LLAPI<FLootLockerGetScoreListResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::GetScoreList, { GetScoreListRequests.leaderboardId, GetScoreListRequests.count },EmptyQueryParams, OnCompletedRequestBP, OnCompletedRequest);
-	}
+	int32 after = GetScoreListRequests.after < 0 ? 0 : GetScoreListRequests.after;
+	LLAPI<FLootLockerGetScoreListResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::GetScoreList, { GetScoreListRequests.leaderboard_key, GetScoreListRequests.count, after }, EmptyQueryParams, OnCompletedRequestBP, OnCompletedRequest);
 }
 
-void ULootLockerLeaderboardRequestHandler::SubmitScore(const FLootLockerSubmitScoreRequest& SubmitScoreRequests, int LeaderboardId, const FLootLockerSubmitScoreResponseBP& OnCompletedRequestBP, const FLootLockerSubmitScoreResponseDelegate& OnCompletedRequest)
+void ULootLockerLeaderboardRequestHandler::SubmitScore(const FLootLockerSubmitScoreRequest& SubmitScoreRequests, FString LeaderboardKey, const FLootLockerSubmitScoreResponseBP& OnCompletedRequestBP, const FLootLockerSubmitScoreResponseDelegate& OnCompletedRequest)
 {
-	LLAPI<FLootLockerSubmitScoreResponse>::CallAPI(HttpClient, SubmitScoreRequests, ULootLockerGameEndpoints::SubmitScore, { LeaderboardId },EmptyQueryParams,OnCompletedRequestBP, OnCompletedRequest);
+	LLAPI<FLootLockerSubmitScoreResponse>::CallAPI(HttpClient, SubmitScoreRequests, ULootLockerGameEndpoints::SubmitScore, { LeaderboardKey }, EmptyQueryParams, OnCompletedRequestBP, OnCompletedRequest);
 }
 
 void ULootLockerLeaderboardRequestHandler::GetAllMemberRanks(const FLootLockerGetAllMemberRanksRequest& GetAllMemberRanksRequests, const FLootLockerGetAllMemberRanksResponseBP& OnCompletedRequestBP, const FLootLockerGetAllMemberRanksResponseDelegate& OnCompletedRequest)
 {
-    TMultiMap<FString,FString> QueryParams;
-    QueryParams.Add("count", FString::FromInt(GetAllMemberRanksRequests.count));
-    if (GetAllMemberRanksRequests.after >= 0)
-    {
-        QueryParams.Add("after", FString::FromInt(GetAllMemberRanksRequests.after));
-    }
-	LLAPI<FLootLockerGetAllMemberRanksResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::GetAllMemberRanks, {GetAllMemberRanksRequests.memberId}, QueryParams, OnCompletedRequestBP, OnCompletedRequest);
+	TMultiMap<FString, FString> QueryParams;
+	QueryParams.Add("count", FString::FromInt(GetAllMemberRanksRequests.count));
+	if (GetAllMemberRanksRequests.after >= 0)
+	{
+		QueryParams.Add("after", FString::FromInt(GetAllMemberRanksRequests.after));
+	}
+	LLAPI<FLootLockerGetAllMemberRanksResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::GetAllMemberRanks, { GetAllMemberRanksRequests.member_id }, QueryParams, OnCompletedRequestBP, OnCompletedRequest);
 }
