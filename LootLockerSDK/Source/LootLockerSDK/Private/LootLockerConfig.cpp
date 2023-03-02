b76ac289e8121c81aa68f061aac02c799c7a0e10
Erik Bylund <erik@lootlocker.io>
2023-03-02 09:33:56 +0100

    Fix build error for UE4.24

--- LootLockerSDK/Source/LootLockerSDK/Private/LootLockerConfig.cpp
+++ LootLockerSDK/Source/LootLockerSDK/Private/LootLockerConfig.cpp
@@ -6,7 +6,11 @@ DEFINE_LOG_CATEGORY(LogLootLockerGameSDK);
 
 FString ULootLockerConfig::GetEnum(const TCHAR* Enum, int32 EnumValue)
 {
+#if ENGINE_MAJOR_VERSION <= 4 && ENGINE_MINOR_VERSION <= 27
+    const UEnum* EnumPtr = FindObject<UEnum>(StaticClass()->GetOuter(), Enum, true);
+#else
     const UEnum* EnumPtr = FindObject<UEnum>(StaticClass()->GetOuterUPackage(), Enum, true);
+#endif
     if (!EnumPtr)
         return NSLOCTEXT("Invalid", "Invalid", "Invalid").ToString();
 
