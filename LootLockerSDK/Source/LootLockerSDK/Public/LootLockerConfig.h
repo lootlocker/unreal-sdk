42e7d0fb021f0dcc60fd09ff52d9b2d3539306b5
Erik Bylund <erik@lootlocker.com>
2024-01-15 10:16:48 +0100

    Help users correctly use the game version setting

--- LootLockerSDK/Source/LootLockerSDK/Public/LootLockerConfig.h
+++ LootLockerSDK/Source/LootLockerSDK/Public/LootLockerConfig.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include <regex>
 #include "CoreMinimal.h"
 #include "LootLockerConfig.generated.h"
 
@@ -10,14 +11,42 @@ class LOOTLOCKERSDK_API ULootLockerConfig : public UObject
 {
 	GENERATED_BODY()
 public:
+
+	UFUNCTION()
+	static bool IsSemverString(const FString& str)
+	{
+		return std::regex_match(TCHAR_TO_UTF8(*str), SemverPattern);
+	}
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override
+	{
+		if (PropertyChangedEvent.GetPropertyName() == "GameVersion")
+		{
+			IsValidGameVersion = IsSemverString(GameVersion);
+		}
+		UObject::PostEditChangeProperty(PropertyChangedEvent);
+	}
+#endif //WITH_EDITOR
+	virtual void PostInitProperties() override
+	{
+		IsValidGameVersion = IsSemverString(GameVersion);
+		UObject::PostInitProperties();
+	}
+
 	// API Key used to talk to LootLocker. The API key can be found in `Settings > API Keys` in the Web Console: https://console.lootlocker.com/settings/api-keys
 	UPROPERTY(Config, EditAnywhere, BlueprintReadWrite, Category = "LootLocker", Meta = (DisplayName = "LootLocker API Key"))
 	FString LootLockerGameKey;
 	UPROPERTY(Config, EditAnywhere, BlueprintReadWrite, Category = "LootLocker")
     FString GameVersion;
+	UPROPERTY(Config, VisibleAnywhere, BlueprintReadOnly, Category = "LootLocker", Meta = (EditCondition = "!IsValidGameVersion", EditConditionHides), Meta = (MultiLine = true), Meta = (DisplayName = "WARNING:"), Transient)
+	FString InvalidGameVersionWarning = "Game version needs to follow a numeric Semantic Versioning pattern: X.Y.Z.B with the sections denoting MAJOR.MINOR.PATCH.BUILD and the last two being optional. Read more at https://docs.lootlocker.com/the-basics/core-concepts/glossary#game-version";
 	UPROPERTY(Config, EditAnywhere, BlueprintReadWrite, Category = "LootLocker")
 	bool AllowTokenRefresh = true;
 	// Domain Key used to talk to LootLocker. The Domain key can be found in `Settings > API Keys` in the Web Console: https://console.lootlocker.com/settings/api-keys
 	UPROPERTY(Config, EditAnywhere, BlueprintReadWrite, Category = "LootLocker")
 	FString DomainKey;
+private:
+	UPROPERTY(Config, VisibleInstanceOnly, Meta = (EditCondition = "false", EditConditionHides), Transient, Category = "LootLocker")
+	bool IsValidGameVersion;
+	inline static const std::regex SemverPattern = std::regex("^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:\\.(0|[1-9]\\d*))?(?:\\.(0|[1-9]\\d*))?$");
 };
