34de5dd5552d83062aaa54ef6ce5f2b80c178568
Erik Bylund <erik@lootlocker.io>
2024-08-20 10:00:09 +0200

    Add support for Retry-After header

--- LootLockerSDK/Source/LootLockerSDK/Public/LootLockerResponse.h
+++ LootLockerSDK/Source/LootLockerSDK/Public/LootLockerResponse.h
@@ -21,6 +21,11 @@ struct FLootLockerErrorData
     // A unique identifier for tracing the request through LootLocker systems, use this in contact with support.
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLocker")
     FString Trace_id;
+    /// <summary>
+    /// If the request was rate limited (status code 429) or the servers were temporarily unavailable (status code 503) you can use this value to determine how many seconds to wait before retrying
+    /// </summary>
+    UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLocker")
+    int Retry_after_seconds = -1;
     // A free text description of the problem and potential suggestions for fixing it
     UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "LootLocker")
     FString Message;
