42f8ba6431bb474e29fe8e34d6fb0caa0306e6f1
Erik Bylund <erik@lootlocker.com>
2024-09-10 13:29:02 +0200

    feat: Add successful request logging as VeryVerbose

--- LootLockerSDK/Source/LootLockerSDK/Public/LootLockerHttpClient.h
+++ LootLockerSDK/Source/LootLockerSDK/Public/LootLockerHttpClient.h
@@ -17,6 +17,7 @@ public:
     void SendApi(const FString& endPoint, const FString& requestType, const FString& data, const FResponseCallback& onCompleteRequest, TMap<FString, FString> customHeaders = TMap<FString, FString>()) const;
     void UploadFile(const FString& endPoint, const FString& requestType, const FString& FilePath, const TMap<FString, FString>& AdditionalFields, const FResponseCallback& onCompleteRequest, TMap<FString, FString> customHeaders = TMap<FString, FString>()) const;
 
+    static void LogSuccessfulRequestInformation(const FLootLockerResponse& Response, const FString& RequestMethod, const FString& Endpoint, const FString& Data);
     static void LogFailedRequestInformation(const FLootLockerResponse& Response, const FString& RequestMethod, const FString& Endpoint, const FString& Data);
 private:
     static bool ResponseIsValid(const FHttpResponsePtr& InResponse, bool bWasSuccessful);
