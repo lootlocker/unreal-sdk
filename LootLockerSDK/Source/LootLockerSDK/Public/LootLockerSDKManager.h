54513fce84c8d9f5d0d030a2ba1d809f8a8741db
Erik Bylund <erik@lootlocker.io>
2024-12-16 21:34:09 +0100

    doc: Fix docs for asset candidates

--- LootLockerSDK/Source/LootLockerSDK/Public/LootLockerSDKManager.h
+++ LootLockerSDK/Source/LootLockerSDK/Public/LootLockerSDKManager.h
@@ -1551,7 +1551,7 @@ public:
     static void CreateAssetCandidate(const FLootLockerCreateAssetCandidateData& AssetCandidateData, const FCreateAssetCandidateResponseDelegate& OnCompletedRequest);
 
     /**
-     * Create an asset candidate and immediately mark it as completed meaning it will become an asset and can not be updated anymore.
+     * Create an asset candidate and immediately mark it as completed
      * https://ref.lootlocker.com/game-api/#creating-an-asset-candidate
      *
      * @param AssetCandidateData asset candidate data.
@@ -1560,7 +1560,7 @@ public:
     static void CreateAssetCandidateAndMarkComplete(const FLootLockerCreateAssetCandidateData& AssetCandidateData, const FCreateAssetCandidateResponseDelegate& OnCompletedRequest);
 
     /**
-     * Update an asset candidate and immediately mark it as completed meaning it will become an asset and can not be updated anymore.
+     * Update an asset candidate
      * https://ref.lootlocker.com/game-api/#updating-an-asset-candidate
      *
      * @param AssetCandidateId ID of the asset candidate.
