5a880c08d9cfb01a19e52fcfa1097d2ecaaa1f1e
Erik Bylund <kirre.bylund@gmail.com>
2025-10-10 11:47:56 +0200

    refactor: Lift blueprint chain of MissionsRequestHandler to Manager
    - Remove blueprint delegate parameters from GetAllMissions, GetMission, StartMission, and FinishMission in RequestHandler
    - Update SDKManager calls to use new RequestHandler signatures
    - Fix Manager to call SDKManager methods with CreateLambda wrappers
    - Maintain three-layer architecture: Manager -> SDKManager -> RequestHandler

--- LootLockerSDK/Source/LootLockerSDK/Private/GameAPI/LootLockerMissionsRequestHandler.cpp
+++ LootLockerSDK/Source/LootLockerSDK/Private/GameAPI/LootLockerMissionsRequestHandler.cpp
@@ -11,22 +11,22 @@ ULootLockerMissionsRequestHandler::ULootLockerMissionsRequestHandler()
     HttpClient = NewObject<ULootLockerHttpClient>();
 }
 
-void ULootLockerMissionsRequestHandler::GetAllMissions(const FLootLockerPlayerData& PlayerData, const FMissionsResponseDelegateBP& OnCompletedRequestBP, const FMissionsResponseDelegate& OnCompletedRequest)
+void ULootLockerMissionsRequestHandler::GetAllMissions(const FLootLockerPlayerData& PlayerData, const FMissionsResponseDelegate& OnCompletedRequest)
 {
-    LLAPI<FLootLockerMissionsResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::GetAllMissionsEndpoint, { }, EmptyQueryParams, PlayerData, OnCompletedRequestBP, OnCompletedRequest);
+    LLAPI<FLootLockerMissionsResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::GetAllMissionsEndpoint, { }, EmptyQueryParams, PlayerData, FMissionsResponseDelegateBP(), OnCompletedRequest);
 }
 
-void ULootLockerMissionsRequestHandler::GetMission(const FLootLockerPlayerData& PlayerData, int MissionId, const FMissionResponseDelegateBP& OnCompletedRequestBP, const FMissionResponseDelegate& OnCompletedRequest)
+void ULootLockerMissionsRequestHandler::GetMission(const FLootLockerPlayerData& PlayerData, int MissionId, const FMissionResponseDelegate& OnCompletedRequest)
 {
-    LLAPI<FLootLockerMissionResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::GetMissionEndpoint, { MissionId }, EmptyQueryParams, PlayerData, OnCompletedRequestBP, OnCompletedRequest);
+    LLAPI<FLootLockerMissionResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::GetMissionEndpoint, { MissionId }, EmptyQueryParams, PlayerData, FMissionResponseDelegateBP(), OnCompletedRequest);
 }
 
-void ULootLockerMissionsRequestHandler::StartMission(const FLootLockerPlayerData& PlayerData, int MissionId, const FStartMissionResponseDelegateBP& OnCompletedRequestBP, const FStartMissionResponseDelegate& OnCompletedRequest)
+void ULootLockerMissionsRequestHandler::StartMission(const FLootLockerPlayerData& PlayerData, int MissionId, const FStartMissionResponseDelegate& OnCompletedRequest)
 {
-    LLAPI<FLootLockerStartMissionResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::StartMissionEndpoint, { MissionId }, EmptyQueryParams, PlayerData, OnCompletedRequestBP, OnCompletedRequest);
+    LLAPI<FLootLockerStartMissionResponse>::CallAPI(HttpClient, LootLockerEmptyRequest, ULootLockerGameEndpoints::StartMissionEndpoint, { MissionId }, EmptyQueryParams, PlayerData, FStartMissionResponseDelegateBP(), OnCompletedRequest);
 }
 
-void ULootLockerMissionsRequestHandler::FinishMission(const FLootLockerPlayerData& PlayerData, int MissionId, const FLootLockerFinishMissionData& MissionData, const FFinishMissionResponseDelegateBP& OnCompletedRequestBP, const FFinishMissionResponseDelegate& OnCompletedRequest)
+void ULootLockerMissionsRequestHandler::FinishMission(const FLootLockerPlayerData& PlayerData, int MissionId, const FLootLockerFinishMissionData& MissionData, const FFinishMissionResponseDelegate& OnCompletedRequest)
 {
-    LLAPI<FLootLockerFinishMissionResponse>::CallAPI(HttpClient, MissionData, ULootLockerGameEndpoints::FinishMissionEndpoint, { MissionId }, EmptyQueryParams, PlayerData, OnCompletedRequestBP, OnCompletedRequest);
+    LLAPI<FLootLockerFinishMissionResponse>::CallAPI(HttpClient, MissionData, ULootLockerGameEndpoints::FinishMissionEndpoint, { MissionId }, EmptyQueryParams, PlayerData, FFinishMissionResponseDelegateBP(), OnCompletedRequest);
 }
