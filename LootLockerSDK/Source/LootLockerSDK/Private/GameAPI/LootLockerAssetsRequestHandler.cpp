e522644800d0d2047c8ba79f801edef29662b752
Erik Bylund <kirre.bylund@gmail.com>
2025-10-23 11:29:46 +0200

    fix: Add Metadata Parsing to Simple Asset Listing

--- LootLockerSDK/Source/LootLockerSDK/Private/GameAPI/LootLockerAssetsRequestHandler.cpp
+++ LootLockerSDK/Source/LootLockerSDK/Private/GameAPI/LootLockerAssetsRequestHandler.cpp
@@ -130,5 +130,100 @@ FString ULootLockerAssetsRequestHandler::ListAssets(const FLootLockerPlayerData&
         QueryParams.Add("order_direction", ULootLockerEnumUtils::GetEnum(TEXT("ELootLockerOrderAssetListDirection"), static_cast<int32>(OrderDirection)));
     }
 
-    return LLAPI<FLootLockerListSimpleAssetsResponse>::CallAPI(Request, ULootLockerGameEndpoints::ListAssetsEndpoint, {}, QueryParams, PlayerData, OnCompletedRequest);
+    LLAPI<FLootLockerListSimpleAssetsResponse>::FResponseInspectorCallback ResponseParser = LLAPI<FLootLockerListSimpleAssetsResponse>::FResponseInspectorCallback::CreateLambda([OnCompletedRequest](FLootLockerListSimpleAssetsResponse& Response)
+    {
+        // Make sure we will have assets to parse before continuing
+        if (!Response.success || Response.assets.Num() == 0)
+        {
+            OnCompletedRequest.ExecuteIfBound(Response);
+            return;
+        }
+        TSharedPtr<FJsonObject> obj = LootLockerUtilities::JsonObjectFromFString(Response.FullTextFromServer);
+        // Ensure that the full response was parsed before continuing
+        if (!obj.IsValid())
+        {
+            OnCompletedRequest.ExecuteIfBound(Response);
+            return;
+        }
+
+        TArray<TSharedPtr<FJsonValue>> JsonAssets = obj.Get()->GetArrayField(TEXT("assets"));
+        // Make sure that the assets array was parsed before continuing
+        if (JsonAssets.Num() != Response.assets.Num())
+        {
+            OnCompletedRequest.ExecuteIfBound(Response);
+            return;
+        }
+
+        auto findCorrectAsset = [](TArray<FLootLockerSimpleAsset>& assets, TArray<TSharedPtr<FJsonValue>>& JsonAssets, int asset_id, int startIndex) -> FLootLockerSimpleAsset*
+        {
+            int i = startIndex;
+            do {
+                FLootLockerSimpleAsset& Asset = assets[i];
+                if (Asset.asset_id == asset_id)
+                {
+                    return &Asset;
+                }
+                i++;
+                if (i >= JsonAssets.Num())
+                {
+                    i = 0;
+                }
+            } while (i != startIndex);
+            return nullptr;
+        };
+
+        auto findCorrectMetadataEntry = [&](FLootLockerSimpleAsset& Asset, const FString& metadataKey, int startIndex) -> FLootLockerMetadataEntry*
+        {
+            int i = startIndex;
+            do {
+                FLootLockerMetadataEntry& MetadataEntry = Asset.metadata[i];
+                if (MetadataEntry.Key.Equals(metadataKey))
+                {
+                    return &MetadataEntry;
+                }
+                i++;
+                if (i >= Asset.metadata.Num())
+                {
+                    i = 0;
+                }
+            } while (i != startIndex);
+            return nullptr;
+        };
+
+        for (int i = 0; i < JsonAssets.Num(); i++)
+        {
+            TSharedPtr<FJsonObject> JsonAssetObject = JsonAssets[i].Get()->AsObject();
+            int AssetId = JsonAssetObject.Get()->GetIntegerField(TEXT("asset_id"));
+            FLootLockerSimpleAsset* AssetPtr = findCorrectAsset(Response.assets, JsonAssets, AssetId, i);
+            if (!AssetPtr)
+            {
+                continue;
+            }
+            FLootLockerSimpleAsset& Asset = *AssetPtr;
+            TArray<TSharedPtr<FJsonValue>> JsonMetadataArray = JsonAssetObject.Get()->GetArrayField(TEXT("metadata"));
+            // Make sure that the metadata array was parsed before continuing
+            if (JsonMetadataArray.Num() != Asset.metadata.Num())
+            {
+                OnCompletedRequest.ExecuteIfBound(Response);
+                return;
+            }
+
+            for (int j = 0; j < JsonMetadataArray.Num(); j++)
+            {
+                TSharedPtr<FJsonObject> JsonMetadataObject = JsonMetadataArray[j].Get()->AsObject();
+                FString MetadataKey = JsonMetadataObject.Get()->GetStringField(TEXT("key"));
+                FLootLockerMetadataEntry* MetadataEntryPtr = findCorrectMetadataEntry(Asset, MetadataKey, j);
+                if (!MetadataEntryPtr)
+                {
+                    continue;
+                }
+                FLootLockerMetadataEntry& MetadataEntry = *MetadataEntryPtr;
+                MetadataEntry._INTERNAL_SetJsonRepresentation(*JsonMetadataObject.Get());
+            }
+        }
+
+        OnCompletedRequest.ExecuteIfBound(Response);
+    });
+
+    return LLAPI<FLootLockerListSimpleAssetsResponse>::CallAPI(Request, ULootLockerGameEndpoints::ListAssetsEndpoint, {}, QueryParams, PlayerData, FListSimpleAssetsResponseDelegate(), ResponseParser);
 }
