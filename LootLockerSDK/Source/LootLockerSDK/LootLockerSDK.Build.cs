94b69a79105dc16b90cdc24e7951c77635b255cb
Erik Bylund <kirre.bylund@gmail.com>
2026-01-08 16:07:58 +0100

    incr: Move to FTSTicker instead of world timers

--- LootLockerSDK/Source/LootLockerSDK/LootLockerSDK.Build.cs
+++ LootLockerSDK/Source/LootLockerSDK/LootLockerSDK.Build.cs
@@ -7,7 +7,6 @@ public class LootLockerSDK : ModuleRules
     public static bool bEnableGoogleSubsystemHelper = false;
     public static bool bUseStageURL = false;
     public static bool bShowOutdatedSDKMessage = false; // Set to true when submitting to fab for engine versions < the last 3
-    public static bool bEnablePresenceFeatures = true; // WebSocket-based presence functionality
     public LootLockerSDK(ReadOnlyTargetRules Target) : base(Target)
     {
         PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
