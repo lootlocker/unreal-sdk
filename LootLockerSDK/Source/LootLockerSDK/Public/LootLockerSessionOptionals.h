1284accdae0e4a6b7d80907b39a27fb88d1fbcca
Erik Bylund <kirre.bylund@gmail.com>
2026-01-22 14:06:24 +0100

    feat: Add support for player name in session optionals

--- LootLockerSDK/Source/LootLockerSDK/Public/LootLockerSessionOptionals.h
+++ LootLockerSDK/Source/LootLockerSDK/Public/LootLockerSessionOptionals.h
@@ -16,9 +16,14 @@ struct FLootLockerSessionOptionals
 	 * Timezone in IANA format. If not supplied, will be set to UTC.
 	 */
 	FString Timezone = "";
+	/**
+	 * The name of the player (same as set by SetPlayerName). If not supplied, will be left blank.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LootLocker")
+	FString Player_name = "";
 
 	bool IsSet() const
 	{
-		return !Timezone.IsEmpty();
+		return !Timezone.IsEmpty() || !Player_name.IsEmpty();
 	}
 };
