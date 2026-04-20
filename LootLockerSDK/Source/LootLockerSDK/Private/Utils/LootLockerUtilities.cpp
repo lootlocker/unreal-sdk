2456da09c9838ddc31469af1fb4b0481110c9c02
Erik Bylund <kirre.bylund@gmail.com>
2026-04-20 13:13:52 +0200

    fix(1327): remove leftover missions include and helpers from LootLockerUtilities

--- LootLockerSDK/Source/LootLockerSDK/Private/Utils/LootLockerUtilities.cpp
+++ LootLockerSDK/Source/LootLockerSDK/Private/Utils/LootLockerUtilities.cpp
@@ -38,53 +38,6 @@ namespace LootLockerUtilities
         return Url + AppendSymbol + Parameter;
     }
 
-    TArray<FLootLockerMissionCheckpoint> ParseMissionCheckpoints(const TSharedPtr<FJsonObject>& MissionJson)
-    {
-        TArray<FLootLockerMissionCheckpoint> Checkpoints;
-        if (MissionJson->HasTypedField<EJson::Array>(TEXT("checkpoints")))
-        {
-            TArray<TSharedPtr<FJsonValue>> CheckPointsJson = MissionJson->GetArrayField(TEXT("checkpoints"));
-            for (int i = 0; i < CheckPointsJson.Num(); i++) {
-                TSharedPtr<FJsonObject> temp = CheckPointsJson[i]->AsObject();
-                TMap<FString, FString> parameters;
-                FLootLockerMissionCheckpoint checkpoint;
-                for (auto FormDataIt = temp->Values.CreateIterator(); FormDataIt; ++FormDataIt)
-                {
-                    FString Key = FormDataIt.Key();
-                    if (Key == "index") {
-                        checkpoint.index = (int)FormDataIt.Value().Get()->AsNumber();
-                    }
-                    else if (Key == "time") {
-                        checkpoint.time = (int)FormDataIt.Value().Get()->AsNumber();
-                    }
-                    else {
-                        parameters.Add(Key, FormDataIt.Value().Get()->AsString());
-                    }
-                }
-                checkpoint.parameters = parameters;
-                Checkpoints.Add(checkpoint);
-            }
-        }
-        
-        return Checkpoints;
-    }
-
-    TArray<TSharedPtr<FJsonValue>> SerializeMissionCheckpoints(const TArray<FLootLockerMissionCheckpoint>& Checkpoints)
-    {
-        TArray<TSharedPtr<FJsonValue>> ItemsJsonArray;
-        for (int i = 0; i < Checkpoints.Num(); i++)
-        {
-	        const TSharedPtr<FJsonObject> JsonCheckPoint = FJsonObjectConverter::UStructToJsonObject(Checkpoints[i]);
-            JsonCheckPoint->RemoveField(TEXT("parameters"));
-            for (auto& KeyValuePair : Checkpoints[i].parameters) {
-                JsonCheckPoint->SetStringField(KeyValuePair.Key, KeyValuePair.Value);
-            }
-            
-            ItemsJsonArray.Push(MakeShareable(new FJsonValueObject(JsonCheckPoint)));
-        }
-        return ItemsJsonArray;
-    }
-
     TSharedPtr<FJsonObject> JsonObjectFromFString(const FString& JsonString)
     {
         TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject());
